8c47b0a6a201259b872c5704d8fc0f74ac236406
yiyuan <yyin18@depaul.edu>
2023-10-10 23:07:55 -0500

    feat: integrate components to app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useState } from 'react'
+import './App.css'
+import Login from './components/Login'
+import { TodoItem } from './components/TodoItem'
+import Logout from './components/Logout'
+import { Button, Modal } from 'react-bootstrap'
+import { NewTodoForm } from './components/NewTodoForm'
+
+const initialToDoLists = [
+    {
+        title: 'buy a bottle of milk',
+        description: 'organic',
+        author: 'Default',
+        dateCreated: new Date('2020-05-12T23:50:21.817Z'),
+        complete: true,
+        dateCompleted: new Date('2020-05-14T23:50:21.817Z'),
+    },
+    {
+        title: 'close the door',
+        description: '',
+        author: 'Default',
+        dateCreated: new Date('2023-10-10T13:50:21.817Z'),
+        complete: true,
+        dateCompleted: new Date('2023-10-10T23:50:21.817Z'),
+    },
+    {
+        title: 'submit homework',
+        description: 'csc436',
+        author: 'Default',
+        dateCreated: new Date('2023-10-10T13:50:21.817Z'),
+        complete: false,
+        dateCompleted: '',
+    },
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [authed, setAuthed] = useState(false)
+    const [name, setName] = useState('')
+    const [todoList, setTodoList] = useState(initialToDoLists)
+    const [show, setShow] = useState(false)
+
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true)
+
+    const onLogin = useCallback((userName) => {
+        setAuthed(true)
+        setName(userName)
+    }, [])
+
+    const onLogOut = useCallback(() => {
+        setAuthed(false)
+        setName('')
+        setTodoList([])
+    }, [])
+
+    const onSubmit = useCallback(
+        (newData) => {
+            handleClose()
+            setTodoList([...todoList, { ...newData, author: name }])
+        },
+        [todoList, name]
+    )
+
+    if (!authed) {
+        return <Login onSubmit={onLogin} />
+    }
+
+    return (
+        <div className="App">
+            <h1>{name}'s Todolist</h1>
+            <div className="operations">
+                <Button onClick={handleShow}>add new Todo</Button>
+                <Logout onLogOut={onLogOut} />
+            </div>
+            <div className="todo-list">
+                {todoList.map((item) => (
+                    <TodoItem item={item} key={item.title} />
+                ))}
+            </div>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add a new Todo</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <NewTodoForm onSubmit={(data) => onSubmit(data)} />
+                </Modal.Body>
+            </Modal>
+        </div>
+    )
 }
 
-export default App;
+export default App
