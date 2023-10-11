e52f86706a9a19a32c10c309386fc70ee80ff96a
yiyuan <yyin18@depaul.edu>
2023-10-10 22:59:46 -0500

    feat: add todo data form

---
+++ src/components/NewTodoForm.jsx
@@ -0,0 +1,26 @@
+import { useCallback, useState } from 'react';
+
+export const NewTodoForm = ({ onSubmit }) => {
+  const [formData, setFormData] = useState({
+    title: '',
+    description: '',
+  })
+
+  const onClickSubmit = useCallback(() => {
+    onSubmit({ ...formData, complete: false, dateCreated: new Date()})
+  }, [formData])
+
+  return (
+    <form onSubmit={(e) => e.preventDefault()} >
+      <div>
+        <label htmlFor="title">title:</label>
+        <input type="text" value={formData.title} onChange={e => setFormData({...formData, title: e.target.value})}  id="title" />
+      </div>
+      <div>
+        <label htmlFor="description">description:</label>
+        <input type="text" value={formData.description} onChange={e => setFormData({...formData, description: e.target.value})}  id="description" />
+      </div>
+      <input type="submit" value="Submit" disabled={!formData.title} onClick={onClickSubmit} />
+    </form>
+  )
+}
